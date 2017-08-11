0f8ddc34cbf5b1c984ef7263807fa782833ca3ee
Marge0101 <Marge0101.ms@gmail.com>
2017-08-11 05:22:03 +0000

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
 
-  host = 'awesomeblog-mari.herokuapp.com'
+  host = 'awesomeblog-marisato.herokuapp.com'
   
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
